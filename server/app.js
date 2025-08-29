a4db95d3cedb0e070b1d90e4ec3960f09899fe63
siddharishika <siddharishika@gmail.com>
2025-08-29 19:17:30 +0530

    Changed PORT

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const User=require('./models/User')
 const cookieParser = require('cookie-parser')
 const session = require('express-session');
 const uri = process.env.MONGO_URI;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const imageKitAuth = require('./utils/imageKit');
 const videoRoutes=require('./apis/videoRoutes')
